d74ba36f3eec08a5cce503f8deeb957173f941ed
sher812 <sher812@aucklanduni.ac.nz>
2022-08-06 17:03:34 +1200

    modified logo.png

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Seong.
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: {text: ''},
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '' },
+    });
+  };
+
+  render() {
+    const {task, tasks} = this.state;
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input onChange={this.handleChange} 
+          value={task.text} type="text" id="taskInput"/>
+          <button type="submit">
+            Add Task
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
